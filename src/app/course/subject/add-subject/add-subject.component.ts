c2cab7f4e1353cf39acf2a8d4ada4da6fe4276eb
SuganyaNiroshan <ssuja1991@gmail.com>
2025-02-20 09:10:08 +0530

    created components in subject folder

---
+++ src/app/course/subject/add-subject/add-subject.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-subject',
+  imports: [],
+  templateUrl: './add-subject.component.html',
+  styleUrl: './add-subject.component.scss'
+})
+export class AddSubjectComponent {
+
+}
---
+++ src/app/course/subject/subject-list/subject-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subject-list',
+  imports: [],
+  templateUrl: './subject-list.component.html',
+  styleUrl: './subject-list.component.scss'
+})
+export class SubjectListComponent {
+
+}
